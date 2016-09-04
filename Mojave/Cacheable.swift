2939ecca71ef431685966663590c8fa9795445b6
Andrew Cavanagh <acavanagh@gmail.com>
2016-09-03 23:47:00 -0700

    testing cacheable

--- Mojave/Cacheable.swift
+++ Mojave/Cacheable.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+public protocol Cacheable {
+    func encode(with: Coder) -> Data
+    init?(with: Coder)
+}
+
+public struct Coder: Transcodeable {
+    private var values = [String : [UInt8]]()
+    
+    mutating func encode<T>(_ value: T, for key: String) {
+        values[key] = toByteArray(value)
+    }
+    func decode<T>(for key: String) -> T? {
+        guard let value = values[key] else { return nil }
+        return fromByteArray(value, T.self)
+    }
+}
+
+extension Coder {
+    fileprivate func toByteArray<T>(_ value: T) -> [UInt8] {
+        var data = [UInt8](repeating: 0, count: MemoryLayout<T>.size)
+        data.withUnsafeMutableBufferPointer {
+            UnsafeMutableRawPointer($0.baseAddress!).storeBytes(of: value, as: T.self)
+        }
+        return data
+    }
+    fileprivate func fromByteArray<T>(_ value: [UInt8], _: T.Type) -> T {
+        return value.withUnsafeBufferPointer {
+            UnsafeRawPointer($0.baseAddress!).load(as: T.self)
+        }
+    }
+}
+
+
+
+
+
+
--- Mojave/Transcodeable.swift
+++ Mojave/Transcodeable.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+public protocol Transcodeable {
+    func encode() -> Data
+    func decode(data: Data) -> Self
+}
+
+public extension Transcodeable {
+    public func encode() -> Data {
+        return Data(bytes: encodeBuffer())
+    }
+    public func decode(data: Data) -> Self {
+        let buffer: [UInt8] = data.withUnsafeBytes {
+            $0.pointee
+        }
+        return decode(buffer: buffer)
+    }
+}
+
+fileprivate extension Transcodeable {
+    func encodeBuffer() -> [UInt8] {
+        var buffer = [UInt8](repeating: 0, count: MemoryLayout<Self>.size)
+        buffer.withUnsafeMutableBufferPointer {
+            UnsafeMutableRawPointer($0.baseAddress!).storeBytes(of: self, as: Self.self)
+        }
+        return buffer
+    }
+    func decode(buffer: [UInt8]) -> Self {
+        return buffer.withUnsafeBufferPointer {
+            UnsafeRawPointer($0.baseAddress!).load(as: Self.self)
+        }
+    }
+}
